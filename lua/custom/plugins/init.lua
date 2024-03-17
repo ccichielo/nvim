36762ab815742759a1662cde108c074e5cc97f99
Christopher Cichielo <chris.cichielo@gmail.com>
2024-03-17 12:00:49 -0400

    treesitter folding + harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set foldmethod to 'expr' and foldexpr to 'nvim_treesitter#foldexpr()' for code folding using TreeSitter
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldcolumn = '0'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
 return {}
