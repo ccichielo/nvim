4f463dc6086295a67bae78c1588d3ed9a88ee6da
Christopher Cichielo <chris.cichielo@gmail.com>
2024-03-17 22:30:13 -0400

    allow delete for harpoon

--- init.lua
+++ init.lua
@@ -532,7 +532,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -607,6 +607,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
