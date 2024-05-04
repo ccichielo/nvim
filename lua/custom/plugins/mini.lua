322760b6343bcbc0be37361dde76421710b2e5bd
Chris Cichielo <chris.cichielo@gmail.com>
2024-05-04 18:18:27 -0400

    mini starter

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -22,6 +22,8 @@ return {
       },
     }
 
+    require('mini.starter').setup()
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
